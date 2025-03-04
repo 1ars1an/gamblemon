47a2925f6f5fe5ff7651ed670f4e2e9b41259bbc
ars1an <arslanmansoor22@gmail.com>
2025-03-04 22:33:35 +0300

    update: cards route receiving data, failed-access-token request interceptor setup to refresh token and attempt api call

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuth } from '../auth';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/users',
+  baseURL: 'http://127.0.0.1:8000/api',
   withCredentials: true,
 });
 
